9d1fcdd06b410019280eccf0f12aed888ef2a010
San <114858611+san-635@users.noreply.github.com>
2024-04-05 15:11:08 +0100

    Delete model/vilt/modules/__init__.py

--- model/vilt/modules/__init__.py
+++
@@ -1,4 +0,0 @@
-from .vilt_module import ViLTransformerSS
-from .vilt_module_ehr_cxr import ViLTransformerSS
-from .vilt_module_ehr_ppg_cxr import ViLTransformerSS_og, ViLTransformerSS_win, ViLTransformerSS_win_cv
-# from .vilt_missing_aware_prompt_module import ViLTransformerSS
